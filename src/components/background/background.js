a5dbb35ba619dc003d4f1efaa207bd56239e4288
sneha-105 <snehawandhare.sss@gmail.com>
2024-12-19 13:29:53 +0530

    added second section

---
+++ src/components/background/background.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import * as styles from './background.module.css'
+
+
+
+export const Background = () => {
+  return (
+    <div >
+      <div className={`container-fluid ${styles.section}`}>
+        <div className={`row ${styles.box}`}>
+          <div className={styles.pageContainer}>
+          <div className={`col-sm-6 ${styles.Background}`}>
+            <div className={`row d-flex `}>
+              <div className={`d-flex ${styles.section1}`}>
+               <div className={styles.bck}>
+                <img className={`${styles.bckImg1}`} src="https://s3-alpha-sig.figma.com/img/3bfd/291b/4690e622b512c8f2b4b5a33c2f60730e?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TZXjXW6oCylvwNHMYFrV3zECcRhS9TxqZow-xzjVcTf5jkRwCjq8N59~89d2Z9AGnyMiFAdkI~PBH1jOpyFvonJrkuxkQG7xiPdU8HIPdgU9GpnuRfIhOYiQAcDt8JOPigMN86h~oPmtq3Z0RZkw5LOWSGibUz0xFraDiDC9vXasPwSRrPs3errTXFUYUeYTth43ql5hBKn9u6hRLb5o0sJ1zhQDtTmAuX0m4VDN0p2pZ7Icc2YgvIiFz-XXPZjXNp9PLdvCqLKvN8AF48D50~7CX4FcMjQ5pZipHpA0tZW4cBhhHPWzmKyrJtZueOtAhi3X2kAFh1KS7aDm0QrpUg__" alt="" />
+              </div>
+              <div className="">
+              <span className={styles.backgroundHeading}>BACKGROUND</span>
+              <p className="backgroundContent">
+              Existing counter-UAS solutions often lack adaptive responses, 
+              scalability, and autonomous engagement capabilities essential for 
+              today’s defense needs
+              </p>
+              </div>
+              </div>
+            </div>
+            <div className={`row d-flex `}>
+            <div className={`d-flex ${styles.section1}`}>
+            <div className={styles.bck}>
+            <img className={`${styles.bckImg1}`}  src="https://s3-alpha-sig.figma.com/img/e5b5/00e9/320554bb635f256bd39a005d040ad120?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=W02Jh63ZAhT~uQZzeYw7D1MR-NuE27YOPObeNqUl8OuEsaGEEiddieDIPweCaLWL04SQQO-PpAbDlr7YARanyoC33QV-PTxxw5f1KlIHvwqJ56nRW5zhvRO9-BYkqlDNIp9lmyKUjbijdn4mX9dzAGSu1QpESkmorVLUXp4Su2VLWgaTnrKnkYtdg~kGm2Yl8C8ZtdcvrtXXaO0QGVgNp9Kx1dkDvxRcIZRGhn89yQPjvvkwmfzRQOI3n~HRq1wU6OzRGAeDI0W4j3PNJ3RG54qrto2fGCSvWKMX597IanD4yBoW4jlziUAzonRPjCqRxHCoYauBoXgy7LqdKJ2Aow__" alt="" />
+            </div>
+            <div>
+            <span className={styles.backgroundHeading}>MARKET OPPORTUNITY</span>
+            <p className="">
+            Global counter-UAS market expected to reach $7.5B by 2031, 
+            with strong demand in border security, defense, 
+            and infrastructure protection. Projected annual revenue of $20M - $48M 
+            through direct sales, AI subscriptions, maintenance contracts, and licensing.
+            </p>
+            </div>
+            </div>
+            </div>
+            <div className={`row`}>
+              <div className={`d-flex ${styles.section1}`}>
+            <div className={styles.bck}>
+              <img className={`${styles.bckImg1}`} src="https://s3-alpha-sig.figma.com/img/08a1/d8ec/0181fa5e042321c69f6cb198c344b873?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=C7aQnXw4qCTiMdlr8YAqsm8f~CZaFZVz6qbzw7x0yq7RVhMfK4ueuhGbq1QMuKOkLsx2yK56Rb8m94Qe7AdKV4zAoL7jHb6kzz2EX0JjDiNDQOrwUVow0MP2ydmyeSgGOn4NaEw8KDNvNm9KG9YSjwml4Wtr-xUHZWOjBWLhTuegSPsfLV4lfo4r6yiKt76aKO2YlRFbaaWNKk3gt6aw9f61zEg~lzwp3gHDOcZ84GEEhY~MQDxIYJxxcuEDGuzSTL4qhXXrtqEkG85UHP4SNqJIhOyDv0rYwU26Huu4OpnRBKPoVFzbhiQ-Fn59-S2ojvI8-U~nDdqqvW-659-MhQ__" alt="img3" />
+            </div>
+            <div>
+            <span className={styles.backgroundHeading}>TEAM</span>
+            <p className="">
+            Defense and AI Experts: Core team with deep experience in defense technology, 
+            AI development, autonomous systems, and UAS countermeasures.
+
+            Proven Leadership: Executives with backgrounds in government defense programs, 
+            AI research, and security operations, ensuring strategic guidance and innovative solutions.
+            </p>
+              </div>
+              </div>
+              </div>
+        
+          </div>
+          <div className={`col-sm-6 ${styles.solution}`}>
+            <div className={styles.solBck}>
+            <img className={styles.solImg} src="https://s3-alpha-sig.figma.com/img/1847/f8e5/26b3ef81b59a6c3fe97b0aa5670b99f4?Expires=1734912000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XQEioF54oPGwQUURh1yr8iCKLqbCX7UxQSzlLjuAbcchwY9sgU56LuJtBg44VhatmiOH-9-bCx0LSf5NjCqn5gcMQ9OYlKpRphAVsbiod2wOShtvK2lrSXe0uTlgiNfk3xdvU5dJbvjfCK-GULQ1DPAyDrhbaExuxDwcaIjo4FAZhfiaZFGJqi7jAR9WIdJlh6xPeAP4W~wKoQY~4XzcZFOiA9XRiSwyOZh5Pes3TxiH4jbbr-15aRwFOzlamWTaUfD6EnWo9BFNuITR3RbTcsppLfh3rSbhYkYcu1QLcXqWjLRCFTHBo~N003C5z-Nd6mew0XcPhsK4T3G7Akkp9g__" alt="solution image"/>
+            </div>
+            <p className={styles.solutionContent}>
+            Our Vajr Solution is an advanced 
+            multi-layered counter-UAS solution 
+            featuring NeuraHive AI, a groundbreaking technology 
+            that autonomously detects, classifies, and responds 
+            to drone threats with real-time precision.
+            </p>
+            <p className={styles.solutionContent}>
+            NeuraHive’s adaptive intelligence 
+            integrates multi-sensor data fusion to 
+            predict and counter complex swarm tactics, 
+            providing unparalleled accuracy and response speed. 
+            Unlike traditional systems, Vajr’s layered defenses—including 
+            targeted GNSS spoofing and autonomous swarm 
+            countermeasures—offer a fully scalable and resilient 
+            solution for critical security environments.
+            </p>
+          </div>
+        </div>
+        </div>
+      </div>
+        
+      </div>
+  )
+}
+
+
+
