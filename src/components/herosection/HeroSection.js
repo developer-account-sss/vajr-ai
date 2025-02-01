fbb2a89967538b6adfd004fab4cb38220c8724a3
ritiksha-sss <ritikshashukla@outlook.com>
2025-02-01 17:45:20 +0530

    Contact form is working, the site is responsive. V1 is MOSTLY READY.

--- src/components/herosection/HeroSection.js
+++ src/components/herosection/HeroSection.js
@@ -3,7 +3,7 @@ import * as styles from './herosection.module.css'
 
 const HeroSection = () => {
     return (
-        <section >
+        <section>
             <div className={`container-fluid `}>
                 <div className="row">
                     <div className={`col-sm-12 ${styles.heroSection}`}>
--- src/pages/first-page.js
+++ src/pages/first-page.js
@@ -1,21 +1,30 @@
 import React from "react";
 import HeroSection from "../components/herosection/HeroSection";
-import { Background } from "../components/background/background";
-import {Problem} from "../components/problem/problem";
-import AttackResponse from "../components/Attack-Response/attackResponse";
-import Pillars from "../components/Pillars/pillars";
+// import { Background } from "../components/background/background";
+// import {Problem} from "../components/problem/problem";
+// import AttackResponse from "../components/Attack-Response/attackResponse";
+// import Pillars from "../components/Pillars/pillars";
+import ContactUs from "../components/contact-us/contactUs";
+import logo from '../images/vajr-logo.png';
 
 
 
 
 const MainPage = () => {
-    return <div>
-        <HeroSection/>
-        <Background/>
-        <Problem/>
-        <AttackResponse />
-        <Pillars />
-    </div>
+    return (
+        <div>
+            <div className="d-flex flex-column header">
+                <h1>Vajr</h1>
+                <img src= {logo} alt="Logo for Vajr.ai" />
+            </div>
+            <HeroSection/>
+            <ContactUs />
+            {/* <Background/>
+            <Problem/>
+            <AttackResponse />
+            <Pillars /> */}
+        </div>
+    )
 }
 
 export default MainPage;
