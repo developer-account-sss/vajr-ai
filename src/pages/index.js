ed0b2c3f3caaa6a014e94029adbcf036a6903e50
ritiksha-sss <ritikshashukla@outlook.com>
2025-02-05 19:45:50 +0530

    Fixed an issue where the header section collided with the Contact Form.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,38 @@
-import React from "react";
-import HeroSection from "../components/herosection/HeroSection";
-// import { Background } from "../components/background/background";
-// import {Problem} from "../components/problem/problem";
-// import AttackResponse from "../components/Attack-Response/attackResponse";
-// import Pillars from "../components/Pillars/pillars";
-import ContactUs from "../components/contact-us/contactUs";
-import logo from '../images/vajr-logo.png';
-// import SplashScreen from "../components/splash-screen/splashScreen";
-
-
-
+import React, { useEffect, useState } from "react"
+import HeroSection from "../components/herosection/HeroSection"
+import ContactUs from "../components/contact-us/contactUs"
+import logo from "../images/vajr-logo.png"
+import SplashScreen from "../components/splash-screen/splashScreen"
 
 const MainPage = () => {
-    return (
-        <div>
-            <div className="d-flex flex-column header">
-                <h1>Vajr</h1>
-                <img src= {logo} alt="Logo for Vajr.ai" />
-            </div>
-            <HeroSection/>
-            {/* <SplashScreen /> */}
-            <ContactUs />
-            {/* <Background/>
-            <Problem/>
-            <AttackResponse />
-            <Pillars /> */}
-        </div>
-    )
+  const [prevScrollPos, setPrevScrollPos] = useState(0)
+  const [visible, setVisible] = useState(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.scrollY
+      setVisible(prevScrollPos > currentScrollPos || currentScrollPos < 10)
+      setPrevScrollPos(currentScrollPos)
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => window.removeEventListener("scroll", handleScroll);
+
+  }, [prevScrollPos])
+
+
+  return (
+    <div>
+      <div className= {`d-flex flex-column header ${visible ? 'show' : 'hide'}`}>
+        <h1>Vajr</h1>
+        <img src={logo} alt="Logo for Vajr.ai" />
+      </div>
+      <HeroSection />
+      <SplashScreen />
+      <ContactUs />
+    </div>
+  )
 }
 
-export default MainPage;
+export default MainPage
